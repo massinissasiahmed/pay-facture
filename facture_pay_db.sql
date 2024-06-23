8846223167775bfb1a7161f7e3a1935afc5f986e
amine-dana <danaamine080807@gmail.com>
2024-06-23 07:47:36 -0400

    update db

---
+++ facture_pay_db.sql
@@ -0,0 +1,91 @@
+-- Create database
+CREATE DATABASE scan_app_db;
+
+-- Create users table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    phone_number VARCHAR(15),
+    email-verified BOOLEAN DEFAULT FALSE,
+    verification_token VARCHAR(255) ,
+    recovery_token VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role VARCHAR(20) NOT NULL, -- 'client', 'admin'
+    deleted BOOLEAN,
+    last_connected_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create clients table
+CREATE TABLE clients (
+    client_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    full_name VARCHAR(100),
+    id_card_scan BYTEA,
+    profile_picture BYTEA, 
+    facial_verification BYTEA,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create folders table
+CREATE TABLE folders (
+    folder_id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES clients(client_id) ON DELETE CASCADE, 
+    folder_name VARCHAR(100),
+    folder_description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create documents table
+CREATE TABLE documents (
+    document_id SERIAL PRIMARY KEY,
+    folder_id INT REFERENCES folders(folder_id) ON DELETE CASCADE,
+    document_name VARCHAR(100),
+    document_description TEXT,
+    document_scan BYTEA,
+    document_details JSONB,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create deposits table
+CREATE TABLE deposits (
+    deposit_id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES clients(client_id) ON DELETE CASCADE,
+    bank_deposit_details JSONB,
+    deposit_slip BYTEA,
+    verified BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create transactions table
+CREATE TABLE transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES clients(client_id) ON DELETE CASCADE,
+    transaction_details JSONB,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create notifications table
+CREATE TABLE notifications (
+    notification_id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES clients(client_id) ON DELETE CASCADE,
+    notification_text TEXT,
+    read_status BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    read_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create security settings table
+CREATE TABLE security_settings (
+    setting_id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES clients(client_id) ON DELETE CASCADE,
+    two_factor_enabled BOOLEAN DEFAULT FALSE,
+    data_encryption_enabled BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
